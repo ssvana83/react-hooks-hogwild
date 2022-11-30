0c745d2c13e6edf7120b4ecb07bc2b3ab5503b35
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2022-11-30 08:50:12 -0500

    Done.

--- src/components/HogContainer.js
+++ src/components/HogContainer.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import HogTile from "./HogTile"
 
 function HogContainer({ hogs }) {
+  const [chosenFilter, setChosenFilter] = useState('all')
+  const renderHogs = filterHogs().map(hog => <HogTile key={hog.name} hog={hog}/>)
+  
 
-  const renderHogs = hogs.map(hog => <HogTile key={hog.name} hog={hog}/>)
+  function handleChange(e){
+    // console.log(e.target.value)
+    setChosenFilter(e.target.value)
+  }
+
+  function filterHogs() {
+    if(chosenFilter === 'greased') {
+      return hogs.filter(hog => hog.greased)
+    } else if(chosenFilter === 'not greased') {
+      return hogs.filter(hog => !hog.greased)
+    } else {
+      return hogs
+    }
+  }
+
   return (
     <div>
+      <select onChange={handleChange}>
+        <option value='all'>all</option>
+        <option value='greased'>greased</option>
+        <option value='not greased'>not greased</option>
+      </select>
       {renderHogs}
     </div>
   )
